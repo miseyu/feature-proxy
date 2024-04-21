34a4c267c4b40f56d7e7bd87bb47514e5a01a441
Yuji Mise <miseyu2000@gmail.com>
2024-04-21 13:52:04 +0900

    feat: Refactor Dockerfile and Makefile, and add health check endpoint in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -16,6 +16,11 @@ func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	slog.SetDefault(logger)
 
+	http.Handle("/proxy/healthz", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = w.Write([]byte(`{"status":"ok"}`))
+		w.WriteHeader(http.StatusOK)
+	}))
 	proxy := pkg.NewReverseProxy(cfg.OriginScheme, cfg.OriginBaseDomain, cfg.DefaultSubDomain, cfg.FeatureHeader)
 	http.Handle("/", proxy)
 	listenHost := fmt.Sprintf(":%v", port)
